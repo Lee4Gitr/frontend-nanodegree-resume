f50b09aaf90436068c4b4e3456e89f06b133fdee
Lee4Gitr <Lee4Gitr@gmail.com>
2016-03-29 15:13:06 -0400

    Added formatted name, role, skills, and jobs.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,12 +1,126 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+	"name" : "Aubrey Mills",
+	"role" : "Web Developer",
+	"contact_info" : {
+		"mobile" : "3347821568",
+		"email" : "lee4gitr@gmail.com",
+		"github" : "lee4gitr",
+		"location" : "Tampa"
+	},
+	"welcome_message" : "Hi! My name is Aubrey and this is my Resume!",
+	"skills" : [
+		"Teaching", "JavaScript", "Git","Programming"
+		],
+	"picture_URL" : "images/AubreyMillsPicture.jpg"
+}
 
-var name = "Aubrey Mills"
-var role = "Web Developer"
 
-var formattedName = HTMLheaderName.replace("%data%", name);
-var formattedRole = HTMLheaderRole.replace("%data%", role);
+var work = {
+	"jobs": [
+		{
+			"employer" : "Dr. Henry Panion III",
+			"title" : "Lifeline",
+			"location" : "Birmingham, AL, US",
+			"dates" : "2012 - 2013",
+			"description" : "Teach children aged 12 - 18 basics of Music Technology in 1 week."
+		},
+		{
+			"employer" : "The University of Alabama in Birmingham",
+			"title" : "Hulsey Recital Hall Worker",
+			"location" : "Birmingham, AL, US",
+			"dates" : "2012 - 2013",
+			"description" : " Run lights and sound for presentations for various departments."
+		},
+		{
+			"employer" : "Self",
+			"title" : "Guitar Instructor",
+			"location" : "Tampa, FL, US",
+			"dates" : "2013 - 2016",
+			"description" : "Teach young and old students guitar. "
+		},
+		{
+			"employer" : "Self",
+			"title" : "Composer",
+			"location" : "Tampa, FL, US",
+			"dates" : "2013 - 2016",
+			"description" : "Discuss project with the patron and develop a composition on their needs."
+		}
+	]
+}
 
+var education = {
+	"schools": [
+		{
+			"name" : "Alabama School of Math and Science",
+			"location" : "Mobile, AL, US",
+			"years" : "2007 - 2008"
+		},
+		{
+			"name" : "Andalusia High School",
+			"location" : "Andalusia, AL, US",
+			"years" : "2005-2007,2008-2009",
+			"degree" : "Advanced Diploma"
+		},
+		{
+			"name" : "University of Alabama at Birmingham",
+			"location" : "Birmingham, AL, US",
+			"degree" : "BA",
+			"major" : "Music Technology Cum Laude with Departmental Honors",
+			"years" : "2009-2013"
+		},
+		{
+			"name" : "Udacity",
+			"location" : "Online",
+			"degree" : "Front End Nanodegree"
+		}
+	]
+}
+
+var projects = {
+	"projects": [
+		{
+			"title" : "Personal Website",
+			"dates" : " 2016",
+			"description" : "My first website.",
+			"imagesURL" : [
+			]
+		}
+	]
+}
+
+formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
 $("#header").prepend(formattedRole);
+formattedName = HTMLheaderName.replace("%data%", "Aubrey Mills");
 $("#header").prepend(formattedName);
+
+if (bio.skills.length>0) {
+	
+	$('#header').append(HTMLskillsStart);
+
+	var formattedSkill = HTMLskills.replace("%data%" , bio.skills[0]);
+	$("#skills").append(formattedSkill); 
+	formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+	$("#skills").append(formattedSkill); 
+	formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
+	$("#skills").append(formattedSkill); 
+	formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
+}
+
+
+function displayWork () {
+	for (job in work.jobs) {
+	$('#workExperience').append(HTMLworkStart);
+
+	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+	var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+	var formattedEmployerTitle = formattedEmployer + formattedTitle;
+	$(".work-entry:last").append(formattedEmployerTitle);
+	var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+	$(".work-entry:last").append(formattedDates);
+
+	var formattedDescription =HTMLworkDescription.replace("%data%", work.jobs[job].description);
+	$(".work-entry:last").append(formattedDescription);
+	}
+}
+
+displayWork();
